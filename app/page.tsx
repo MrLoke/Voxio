6b6a4e5734b8b187ac99df0970b5a82cb2501a6e
Michal Grzesiak <darkjjookkeerr@gmail.com>
2025-11-26 00:33:39 +0100

    style: add responsive design for home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,38 +6,40 @@ import { SelectLanguage } from "@/components/SelectLanguage/SelectLanguage";
 
 const Index = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-zinc-900 text-zinc-50">
+    <div className="flex flex-col items-center bg-slate-900 text-slate-50">
       <AuroraBackground />
 
-      <div className="flex w-full items-center px-8 justify-end">
+      <div className="flex w-full items-center px-8 mb-12 justify-end">
         <SelectLanguage />
 
         <ModeToggle />
       </div>
-      <h1 className="text-6xl font-extrabold mb-4 text-transparent bg-clip-text bg-linear-to-r from-zinc-200 to-zinc-400">
-        Witaj w VOXIO
-      </h1>
-      <p className="text-2xl text-zinc-400 max-w-2xl text-center mb-10">
-        VOXIO to nowoczesna platforma do prowadzenia czatów głosowych i wideo,
-        idealna do spotkań towarzyskich i małych grup roboczych. Połącz się ze
-        znajomymi w wirtualnych pokojach, bez instalacji i opóźnień.
-      </p>
-      <div className="flex space-x-4">
-        <Link href={SIGNUP_ROUTE}>
-          <button className="bg-zinc-600 hover:bg-zinc-700 text-white font-bold py-3 px-8 rounded-lg text-lg transition-colors">
-            Zarejestruj się
-          </button>
-        </Link>
-        <Link href={SIGNIN_ROUTE}>
-          <button className="border border-zinc-600 text-zinc-400 hover:text-white hover:bg-zinc-800 font-bold py-3 px-8 rounded-lg text-lg transition-colors">
-            Zaloguj się
-          </button>
-        </Link>
-      </div>
 
-      <p className="mt-12 text-zinc-500">
-        [Tutaj można dodać więcej sekcji opisujących funkcjonalność]
-      </p>
+      <section className="flex flex-col items-center min-h-screen bg-slate-900 text-slate-50 px-8">
+        <h1 className="text-3xl text-center sm:text-4xl md:text-6xl font-extrabold mb-4 font-suse text-transparent bg-clip-text bg-linear-to-r from-slate-200 to-slate-400">
+          Welcome to VOXIO
+        </h1>
+        <p className="text-base sm:text-lg md:text-2xl font-suse text-slate-400 max-w-2xl text-center mb-10">
+          VOXIO is a modern platform for chatting and seamless voice & video
+          conversations, designed for communities, teams, and spontaneous
+          meetups. Join virtual rooms, talk without interruptions, and connect
+          with others — with no delays and no unnecessary complexity.
+        </p>
+        <div className="flex justify-center items-center flex-col sm:flex-row sm:space-x-4 gap-3 sm:gap-0">
+          <Link href={SIGNUP_ROUTE} className="w-full sm:w-auto">
+            <button className="w-full sm:w-auto bg-slate-600 hover:bg-slate-700 text-slate-50 text-sm sm:text-base font-bold py-3 px-6 md:py-4 md:px-8 rounded-lg md:text-lg transition-colors">
+              Create new account
+            </button>
+          </Link>
+          <Link href={SIGNIN_ROUTE} className="w-full sm:w-auto">
+            <button className="w-full sm:w-auto border border-slate-600 text-slate-400 hover:text-slate-50 hover:bg-slate-800 text-sm sm:text-base font-bold py-3 px-6 md:py-4 md:px-8 rounded-lg md:text-lg transition-colors">
+              Sign in to VOXIO
+            </button>
+          </Link>
+        </div>
+
+        <p className="mt-12 text-slate-500">More content...</p>
+      </section>
     </div>
   );
 };
