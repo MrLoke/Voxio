632a56c57dffc7ccda3a3947d26490cdd6faa696
Michal Grzesiak <darkjjookkeerr@gmail.com>
2025-11-26 00:21:50 +0100

    feat: add new createBrowserClient for correct operation of chat with websockets

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,4 +4,27 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const createClient = () =>
-  createBrowserClient(supabaseUrl, supabaseAnonKey);
+  createBrowserClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      getAll() {
+        if (typeof window !== "undefined") {
+          const cookies = document.cookie.split(";");
+          return cookies.map((cookie) => {
+            const [name, value] = cookie.trim().split("=", 2);
+            return { name, value: value || "" };
+          });
+        }
+        return [];
+      },
+
+      setAll(cookiesToSet) {
+        if (typeof window !== "undefined") {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            document.cookie = `${name}=${value}; path=${
+              options.path
+            }; max-age=${options.maxAge}; secure=${options.secure ?? true}`;
+          });
+        }
+      },
+    },
+  });
