211bceabadde6c4191125182b3feb9ac954b9af3
Michal Grzesiak <darkjjookkeerr@gmail.com>
2025-11-25 02:14:03 +0100

    feat: add callback route for exchange code for session

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { AUTH_ERROR_REDIRECT_PATH, DASHBOARD_ROUTE } from "@/lib/constants";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? DASHBOARD_ROUTE;
+
+  if (code) {
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          },
+        },
+      }
+    );
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    }
+  }
+
+  // Redirect to error page with the error parameter
+  return NextResponse.redirect(
+    `${origin}${AUTH_ERROR_REDIRECT_PATH}Invalid or expired verification link.`
+  );
+}
