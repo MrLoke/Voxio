5eddaa23cd12e249e4c57aa50dab9a65946bad1e
Michal Grzesiak <darkjjookkeerr@gmail.com>
2025-11-25 02:26:26 +0100

    fix: move exchangeCodeForSession to auth callback and fix that middleware will send the old token to Supabase

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createServerClient } from "@supabase/ssr";
 import { updateSession } from "./lib/supabase/updateSession";
 
 import {
@@ -11,48 +10,9 @@ import {
 } from "@/lib/constants";
 
 export async function middleware(request: NextRequest) {
-  let response = await updateSession(request);
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        // Set cookies for temporary response, main cookies are set in updateSession
-        setAll(cookies) {
-          cookies.forEach(({ name, value, options }) => {
-            response.cookies.set(name, value, options);
-          });
-        },
-      },
-    }
-  );
+  const { response, user } = await updateSession(request);
 
   const pathname = request.nextUrl.pathname.replace(/\/+$/, "") || "/";
-
-  // Redirect instant to /dashboard after verifying email because
-  // after clicking the link you got code in the url like this /signin?code=...
-
-  const code = request.nextUrl.searchParams.get("code");
-
-  if (code && [SIGNIN_ROUTE, SIGNUP_ROUTE].includes(pathname)) {
-    await supabase.auth.exchangeCodeForSession(code);
-
-    const url = request.nextUrl.clone();
-    url.searchParams.delete("code");
-    url.pathname = DASHBOARD_ROUTE;
-
-    response = NextResponse.redirect(url, { headers: response.headers });
-
-    return response;
-  }
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
 
   const isProtectedRoutes = !PUBLIC_ROUTES.includes(pathname);
 
