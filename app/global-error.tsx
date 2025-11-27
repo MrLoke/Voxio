f1346e3e70f557df5467aed74f7f4ce81cfa5725
Michal Grzesiak <darkjjookkeerr@gmail.com>
2025-11-27 21:38:14 +0100

    fix: add root global error for use client component

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -8,13 +8,20 @@ const GlobalError = ({
   reset: () => void;
 }) => {
   return (
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <p>{error.message || "An unexpected error occurred."}</p>
-        <button onClick={() => reset()}>Try again</button>
-      </body>
-    </html>
+    <div className="h-screen flex flex-col items-center pt-24 text-slate-200 bg-slate-900">
+      <h1 className="text-4xl font-bold">Something went wrong</h1>
+
+      <p className="mt-4 text-slate-200">
+        {error.message || "An unexpected error occurred."}
+      </p>
+
+      <button
+        onClick={() => reset()}
+        className="mt-6 border-2 rounded-md px-4 py-2 text-slate-300"
+      >
+        Try Again
+      </button>
+    </div>
   );
 };
 
