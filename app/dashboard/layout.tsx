dc7fd8748db61075472c94a4d47d822aaacbece9
Michal Grzesiak <darkjjookkeerr@gmail.com>
2025-12-11 19:58:27 +0100

    feat: add custom trigger for open sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,20 +1,21 @@
 import { cookies } from "next/headers";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
+import { CustomTrigger } from "@/components/Sidebar/CustomTrigger";
 
-const Layout = async ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
 
   return (
     <SidebarProvider defaultOpen={defaultOpen}>
       <Sidebar />
-      <main className="flex w-full flex-1 flex-col bg-slate-300 text-slate-900 dark:text-slate-100 dark:bg-slate-900">
-        <SidebarTrigger />
+      <main className="flex w-full flex-1 flex-col bg-slate-300 text-slate-900 dark:text-slate-100 dark:bg-slate-800">
+        <CustomTrigger className="md:hidden" />
         {children}
       </main>
     </SidebarProvider>
   );
 };
 
-export default Layout;
+export default DashboardLayout;
